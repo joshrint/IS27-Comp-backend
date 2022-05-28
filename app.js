afb43ea5a63b7a873d4dfa75458bad4ad7df9e9c
joshrint <josh.rint@gmail.com>
2022-05-28 10:58:41 -0700

    Route Cleanup

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use("/boatAPI", boatRouter);
 app.use('/guidesAPI', guideRouter);
 
